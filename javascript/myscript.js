4d19f7b176d3a084fcc56a88f8938f060242c3f6
Daniele <danlup94@gmail.com>
2020-10-22 17:02:28 +0200

    Fix codice js

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -4,69 +4,34 @@ var chilometri = document.getElementById('distanza');
 
 var eta = document.getElementById('age');
 
-var prezzo = chilometri * 0.21;
-
-var percentuale18 = 20
-
-var percentuale65 = 40
-
-var scontoUnder = (percentuale18 / 100) * prezzo
-
-var scontoOver = (percentuale65 / 100) * prezzo
-
-
 
 // BOTTONE GENERA BIGLIETTO
 var bottone = document.getElementById('genera');
 
 // STAMPA NOME PASSEGGERO
-bottone.addEventListener('click',
-function() {
+bottone.addEventListener('click', function() {
   ticket1.innerHTML = identita.value;
-
-} );
-
-// // CALCOLO PREZZO
-// function() {
-//
-//   if (eta < 18){
-//   prezzo = (prezzo - scontoUnder );
-// } else if (eta > 65) {
-//   prezzo = (prezzo - scontoOver );
-//
-// }
-//
-// } );
-
-// GENERA NUMERO CARROZZA
-bottone.addEventListener('click',
-function() {
+  ticket2.innerHTML = 
   ticket3.innerHTML = Math.floor(Math.random() * 21);
-
-} );
-
-// GENERA CODICE
-bottone.addEventListener('click',
-function() {
   ticket4.innerHTML = Math.floor(Math.random() * 101);
 
-} );
+  var prezzo = chilometri.value * 0.21;
 
+  var percentuale18 = 20
 
-// STAMPA PREZZO
-bottone.addEventListener('click',
-function() {
+  var percentuale65 = 40
+
+  var scontoUnder = (percentuale18 / 100) * prezzo
+
+  var scontoOver = (percentuale65 / 100) * prezzo
+
   ticket5.innerHTML = prezzo;
 
-} );
-
-
-
-// FUNZIONI STAMPA RESET
-function stampaTicket() {
   document.getElementById('Ticket').style.display = "block";
-}
+});
 
-function resetTicket() {
+//  RESET
+var annulla = document.getElementById('annulla');
+annulla.addEventListener('click', function() {
   document.getElementById('Ticket').style.display = "none";
-}
+});
